4ba9a10a4c1f8c27dfcbfe3bae84b5014a7ed9f2
Ondřej Pavela <pavela.ondrej@gmail.com>
2020-09-28 21:38:03 +0200

    Model changes + Refactor Switched to class based views with GET/POST actions Added model for confirmed cases, but the import is not implemented yet.

--- covid/covid_app/models.py
+++ covid/covid_app/models.py
@@ -17,10 +17,33 @@ class County(models.Model):
         return f"{self.lau1_code}: {self.name}"
 
 
+class Country(models.Model):
+    csu_code = models.CharField(max_length=2, primary_key=True)
+    name = models.CharField(max_length=80)
+
+    def __str__(self):
+        return f"{self.csu_code}: {self.name}"
+
+
+class ConfirmedCase(models.Model):
+    report_date = models.DateField()
+    age = models.PositiveSmallIntegerField('Age at the time of the report')
+    gender = models.CharField(max_length=1)
+    associated_region = models.ForeignKey(Region, on_delete=models.CASCADE)
+    associated_county = models.ForeignKey(County, on_delete=models.CASCADE)
+    origin_country = models.ForeignKey(Country, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"[Case: {self.report_date}] {self.gender} - {self.age}"
+
+
 class Deceased(models.Model):
     date_of_death = models.DateField()
     age = models.PositiveSmallIntegerField('Person\'s age at death')
     gender = models.CharField(max_length=1)
-    associated_region = models.ForeignKey(Region, on_delete=models.SET_NULL, null=True)
-    associated_county = models.ForeignKey(County, on_delete=models.SET_NULL, null=True)
+    associated_region = models.ForeignKey(Region, on_delete=models.CASCADE)
+    associated_county = models.ForeignKey(County, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"[Deceased: {self.date_of_death}] {self.gender} - {self.age}"
 
--- covid/covid_app/urls.py
+++ covid/covid_app/urls.py
@@ -1,10 +1,30 @@
 from django.urls import path
+from django.views.generic import RedirectView
 
 from . import views
 
 app_name = "covid_app"
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('clear_db/', views.clear_db, name='clear_db'),
+    path('', RedirectView.as_view(pattern_name='covid_app:index', permanent=False)),
+    path('index/', views.IndexView.as_view(), name='index'),
+
+    # path('clear_db/', views.clear_db, name='clear_db'),
+
+    # path('index/', views.GamesView.as_view(), name='index'),
+    # path('login/', views.LoginView.as_view(), name='login'),
+    # path('logout/', views.logout_view, name='logout'),
+    # path('signup/', views.SignupView.as_view(), name='signup'),
+    # path('genre/<slug:slug>/', views.GenreDetailView.as_view(), name='genre_detail'),
+    # path('player/<slug:slug>/', views.PlayerDetailView.as_view(), name='player_detail'),
+    # path('team/<slug:slug>/', views.TeamDetailView.as_view(), name='team_detail'),
+    # path('clan/<slug:slug>/', views.ClanDetailView.as_view(), name='clan_detail'),
+    # path('game/<slug:slug>/', views.GameDetailView.as_view(), name='game_detail'),
+    # path('match/<pk>/', views.MatchDetailView.as_view(), name='match_detail'),
+    # path('games/', views.GamesView.as_view(), name='games'),
+    # path('social/', views.SocialView.as_view(), name='social'),
+    # path('settings/', views.SettingsView.as_view(), name='settings'),
+    # path('tournaments/', views.TournamentView.as_view(), name='tournaments'),
+    # path('tournament/<slug:slug>/', views.TournamentDetailView.as_view(), name='tournament_detail'),
+    # path('gamemode/<slug:slug>/', views.GameModeDetailView.as_view(), name='game_mode_detail')
 ]
